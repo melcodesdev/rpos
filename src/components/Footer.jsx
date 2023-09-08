108f453323e0b86652784aca01dc6aa0c997e439
melcodesdev <137898104+melcodesdev@users.noreply.github.com>
2023-09-09 00:36:02 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <section className='m-4 text-xs opacity-50'>
-      <div className='flex flex-row justify-between my-2'>
+    <section className='m-4 xs:mx-16 text-xs sm:text-base lg:text-lg opacity-50'>
+      <div className='flex flex-row justify-between my-2 sm:my-4'>
         <h4>Features</h4>
         <h4>Testimonials</h4>
         <h4>Pricing</h4>
@@ -12,14 +12,14 @@ const Footer = () => {
 
       <hr />
 
-      <div className='flex flex-row justify-between my-2'>
+      <div className='flex flex-row justify-between my-2 sm:my-4 lg:my-8'>
         <h4>Rpos, Inc. 2023</h4>
 
         <div className='flex flex-row'>
-          <img className='w-5 h-5 ml-2' src="/images/instagram.png" alt="Instagram icon" />
-          <img className='w-5 h-5 ml-2' src="/images/twitter.png" alt="Twitter icon" />
-          <img className='w-5 h-5 ml-2' src="/images/facebook.png" alt="Facebook icon" />
-          <img className='w-5 h-5 ml-2' src="/images/linkedin.png" alt="Linkedin icon" />
+          <img className='w-5 sm:w-8 h-5 sm:h-8 ml-2 sm:ml-4' src="/images/instagram.png" alt="Instagram icon" />
+          <img className='w-5 sm:w-8 h-5 sm:h-8 ml-2 sm:ml-4' src="/images/twitter.png" alt="Twitter icon" />
+          <img className='w-5 sm:w-8 h-5 sm:h-8 ml-2 sm:ml-4' src="/images/facebook.png" alt="Facebook icon" />
+          <img className='w-5 sm:w-8 h-5 sm:h-8 ml-2 sm:ml-4' src="/images/linkedin.png" alt="Linkedin icon" />
         </div>
       </div>
 
