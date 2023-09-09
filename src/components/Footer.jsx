7db7285ac69a3ab5785987e1a21a1beb5684f488
melcodesdev <137898104+melcodesdev@users.noreply.github.com>
2023-09-09 23:46:45 +0700

    Mobile + desktop responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <section className='m-4 xs:mx-16 text-xs sm:text-base lg:text-lg opacity-50'>
-      <div className='flex flex-row justify-between my-2 sm:my-4'>
+    <footer className='m-4 xs:mx-16 xl:mt-32 text-xs sm:text-base lg:text-lg opacity-50'>
+      <div className='flex flex-row justify-between my-2 sm:my-4 lg:mx-56 xl:mx-96'>
         <h4>Features</h4>
         <h4>Testimonials</h4>
         <h4>Pricing</h4>
         <h4>Contact</h4>
       </div>
 
-      <hr />
+      <hr className='xl:mx-28' />
 
-      <div className='flex flex-row justify-between my-2 sm:my-4 lg:my-8'>
+      <div className='flex flex-row justify-between my-2 sm:my-4 lg:my-8 xl:mx-28'>
         <h4>Rpos, Inc. 2023</h4>
 
         <div className='flex flex-row'>
@@ -23,7 +23,7 @@ const Footer = () => {
         </div>
       </div>
 
-    </section>
+    </footer>
   )
 }
 
