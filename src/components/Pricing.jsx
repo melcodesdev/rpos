c2300c1f2755f120059c4c1035bdb157985d9bab
melcodesdev <137898104+melcodesdev@users.noreply.github.com>
2023-09-09 23:48:04 +0700

    Mobile + desktop responsive

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Pricing = () => {
     return (
-        <section className='flex flex-col w-screen bg-gradient-to-b from-fuchsia-500 to-rose-400 font-inter'>
+        <section className='xl:h-screen flex flex-col w-screen bg-gradient-to-b from-fuchsia-500 to-rose-400 font-inter'>
             <h1 className='text-xl sm:text-4xl text-white font-semibold mx-auto mt-8 sm:mt-16 px-4'>Sold? Get started today</h1>
             <h3 className='text-xs sm:text-lg  text-white font-medium mx-auto mb-10 px-4'>Best offers, reasonable prices</h3>
 
-            <section>
-                <div className='w-60 sm:w-3/5 lg:w-1/2 flex flex-col text-white border-2 border-white rounded-lg lg:rounded-3xl mx-auto p-2 sm:p-8'>
+            <section className='xl:flex xl:flex-row xl:mb-12'>
+                <div className='w-60 sm:w-3/5 lg:w-1/2 xl:w-[30vw] flex flex-col text-white border-2 border-white rounded-lg lg:rounded-3xl mx-auto xl:mt-2 p-2 sm:p-8'>
                     <h2 className='text-xl sm:text-2xl font-bold mx-auto'>Starter</h2>
                     <h1 className='text-4xl sm:text-6xl font-bold mx-auto my-2'>$19</h1>
                     <h4 className='text-xs sm:text-lg mx-auto'>For essential business operations</h4>
 
-                    <button className='w-52 sm:w-80 lg:w-96 h-8 sm:h-10 bg-white rounded-md text-sm sm:text-lg text-black font-medium mx-auto my-2'>Get started</button>
+                    <button className='w-52 sm:w-80 lg:w-96 xl:w-80 h-8 sm:h-10 bg-white rounded-md text-sm sm:text-lg text-black font-medium mx-auto my-2'>Get started</button>
 
-                    <div className='lg:ml-8'>
+                    <div className='w-52 sm:w-80 lg:w-96 xl:w-80 mx-auto'>
                         <div className='flex flex-row mt-2'>
                             <img className='w-2.5 sm:w-5 h-2.5 sm:h-5 my-auto' src="/images/checklist.png" alt="Checklist icon" />
                             <h3 className='text-xs sm:text-lg font-medium ml-2 my-auto'>Real time dashboard</h3>
@@ -47,14 +47,14 @@ const Pricing = () => {
                     </div>
                 </div>
 
-                <div className='w-60 sm:w-3/5 lg:w-1/2 flex flex-col text-white border-4 border-white rounded-lg lg:rounded-3xl mx-auto my-8 p-2 sm:p-8'>
+                <div className='w-60 sm:w-3/5 lg:w-1/2 xl:w-[30vw] flex flex-col text-white border-4 border-white rounded-lg lg:rounded-3xl mx-auto my-8 xl:my-0 xl:mt-2 p-2 sm:p-8'>
                     <h2 className='text-xl sm:text-2xl font-bold mx-auto'>Pro</h2>
                     <h1 className='text-4xl font-bold mx-auto my-2'>$49</h1>
                     <h4 className='text-xs sm:text-lg mx-auto'>For scaled business management</h4>
 
-                    <button className='w-52 sm:w-80 lg:w-96 h-8 sm:h-10 bg-white rounded-md text-sm sm:text-lg text-black font-medium mx-auto my-2'>Get started</button>
+                    <button className='w-52 sm:w-80 lg:w-96 xl:w-80 h-8 sm:h-10 bg-white rounded-md text-sm sm:text-lg text-black font-medium mx-auto my-2'>Get started</button>
 
-                    <div className='lg:ml-8'>
+                    <div className='w-52 sm:w-80 lg:w-96 xl:w-80 mx-auto'>
                         <div className='flex flex-row mt-2'>
                             <img className='w-2.5 sm:w-5 h-2.5 sm:h-5 my-auto' src="/images/checklist.png" alt="Checklist icon" />
                             <h3 className='text-xs sm:text-lg font-medium ml-2 my-auto'>Real time dashboard and report</h3>
@@ -92,14 +92,14 @@ const Pricing = () => {
                     </div>
                 </div>
 
-                <div className='w-60 sm:w-3/5 lg:w-1/2 flex flex-col text-white border-2 border-white rounded-lg lg:rounded-3xl mx-auto mt-8 mb-16 p-2 sm:p-8'>
+                <div className='w-60 sm:w-3/5 lg:w-1/2 xl:w-[30vw] flex flex-col text-white border-2 border-white rounded-lg lg:rounded-3xl mx-auto mt-8 xl:mt-2 mb-16 xl:mb-0 p-2 sm:p-8'>
                     <h2 className='text-xl sm:text-2xl font-bold mx-auto'>Enterprise</h2>
                     <h1 className='text-4xl font-bold mx-auto my-2'>$99</h1>
                     <h4 className='text-xs sm:text-lg mx-auto'>Elevate your business to a higher level</h4>
 
-                    <button className='w-52 sm:w-80 lg:w-96 h-8 sm:h-10 bg-white rounded-md text-sm sm:text-lg text-black font-medium mx-auto my-2'>Get started</button>
+                    <button className='w-52 sm:w-80 lg:w-96 xl:w-80 h-8 sm:h-10 bg-white rounded-md text-sm sm:text-lg text-black font-medium mx-auto my-2'>Get started</button>
 
-                    <div className='lg:ml-8'>
+                    <div className='w-52 sm:w-80 lg:w-96 xl:w-80 mx-auto'>
                         <div className='flex flex-row mt-2'>
                             <img className='w-2.5 sm:w-5 h-2.5 sm:h-5 my-auto' src="/images/checklist.png" alt="Checklist icon" />
                             <h3 className='text-xs sm:text-lg font-medium ml-2 my-auto'>Real time dashboard and analysis</h3>
