e0dd1bcf1382e552511aa58a4add25c9ca190cce
melcodesdev <137898104+melcodesdev@users.noreply.github.com>
2023-09-12 03:05:57 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Homepage from './components/Homepage';
 import Overview from './components/Overview';
-import Product from './components/Product';
+import Features from './components/Features';
 import Testimonials from './components/Testimonials';
 import Pricing from './components/Pricing';
 import Faq from './components/Faq';
 import Footer from './components/Footer';
 
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <Homepage />
-    <Overview />
-    <Product />
-    <Testimonials />
-    <Pricing />
-    <Faq />
-    <Footer />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+
+  function Home() {
+    return (
+      <>
+        <Homepage />
+        <Overview />
+        <Features />
+        <Testimonials />
+        <Pricing />
+        <Faq />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App
