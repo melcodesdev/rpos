bbcbcbc8d44eb235c811dddef95bfbe4fc25e7de
Melissa <137898104+melcodesdev@users.noreply.github.com>
2024-02-01 14:27:23 +0700

    refactor + comments

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Overview = () => {
 
     return (
-        // Overview section
+
         <section className='flex flex-col justify-center min-h-screen font-inter mt-4 mb-8'>
             <h1 className='text-2xl xs:text-3xl lg:text-5xl font-semibold mx-auto my-8 sm:mt-24 md:mt-0 lg:my-16 xl:hidden'>Why <span className='text-fuchsia-500'>Rpos</span>? </h1>
 
-            {/* Promotion cards */}
+            {/* PROMOTION CARDS */}
             <section className='xl:flex xl:flex-col'>
                 <div className='xl:flex-1 xl:flex xl:flex-row m-4 lg:m-8 xl:m-16 xl:mx-auto xl:px-[10vw]'>
                     <img className='w-20 xs:w-36 lg:w-56 xl:w-auto xl:h-[80vh] rounded-lg mx-auto xl:mx-0 xl:mr-16' src="/images/features_1.jpg" alt="Chef plating a meal" />
